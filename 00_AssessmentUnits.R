61b3990fc6a585fe0142beafdb0393ae87e8d5a8
Hjalte Parner <hjalte@ices.dk>
2018-10-24 15:48:52 +0200

    Added Dutch assessment units

--- 00_AssessmentUnits.R
+++ 00_AssessmentUnits.R
@@ -1,23 +1,36 @@
 # OSPAR Assessment Units -------------------------------------------------------
 
-# Read SE Assessment Units
+# SE Assessment Units ----------------------------------------------------------
+
+# Read Assessment Units
 OSPAR_Assessment_Units_SE <- sf::st_read("Input/COMP_SE_area.shp")
 
-# Reproject SE Assessment Units into WGS1984 
+# Reproject Assessment Units into WGS1984 
 OSPAR_Assessment_Units_SE <- sf::st_transform(OSPAR_Assessment_Units_SE, crs = 4326)
 
-# Write SE Assessment Units into Assessment Database
+# Write Assessment Units into Assessment Database
 sf::st_write(OSPAR_Assessment_Units_SE, "MSSQL:server=SQL08;database=OceanCOMP_20062014;trusted_connection=yes;", "AssessmentUnitSE")
 
-# Read GE Assessment Units
+# GE Assessment Units ----------------------------------------------------------
+
+# Read Assessment Units
 OSPAR_Assessment_Units_GE <- sf::st_read("Input/comp3_GE_gesamt.shp")
 
-# Write GE Assessment Units into Assessment Database
+# Write Assessment Units into Assessment Database
 sf::st_write(OSPAR_Assessment_Units_GE, "MSSQL:server=SQL08;database=OceanCOMP_20062014;trusted_connection=yes;", "AssessmentUnitGE")
+
+# NL Assessment Units ----------------------------------------------------------
+
+# Read Assessment Units
+OSPAR_Assessment_Units_NL <- sf::st_read("Input/OSPAR_Eutrophication_Pg_NL.shp")
+
+# Write Assessment Units into Assessment Database
+sf::st_write(OSPAR_Assessment_Units_NL, "MSSQL:server=SQL08;database=OceanCOMP_20062014;trusted_connection=yes;", "AssessmentUnitNL")
 
 # Combine Assessment Units within the OSPAR Assessment Database
 # SELECT [OGR_Geometry], 'GE' AS [Country], [Area] AS [Description], [Assessment] AS [Classification] FROM [AssessmentUnitGE]
 # SELECT [OGR_Geometry], 'SE' AS [Country], [Typ] AS [Description], [Assessment] AS [Classification] FROM [AssessmentUnitSE]
+# SELECT [OGR_Geometry], 'NL' AS [Country], [Comp_Name] AS [Description], [Assessment] AS [Classification] FROM [AssessmentUnitNL]
 
 # Read combined OSPAR Assessment Units
 OSPAR_Assessment_Units <- sf::st_read("MSSQL:server=SQL08;database=OceanCOMP_20062014;trusted_connection=yes;", "AssessmentUnit")
